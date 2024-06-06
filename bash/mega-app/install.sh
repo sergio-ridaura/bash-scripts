b856ca4b155e5d1249a823615536ba56ba5d6f24
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-06 22:31:24 +0200

    Adding Bash Scripts

--- bash/mega-app/install.sh
+++ bash/mega-app/install.sh
@@ -2,11 +2,12 @@
 
 LAYOUT_NAME="mega"
 LAYOUT_APP="Mega"
-LAYOUT_VERSION="24.06.05"
+LAYOUT_VERSION="24.06.07"
 LAYOUT_SEE="https://mega.io/"
 
 LAYOUT_CODE() {
-  $IS_ROOT wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb && sudo apt $ARG_YES install "$PWD/megasync-xUbuntu_22.04_amd64.deb"
+  $IS_ROOT apt $ARG_YES install wget
+  $IS_ROOT wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb && $IS_ROOT apt $ARG_YES install "$PWD/megasync-xUbuntu_22.04_amd64.deb"
 }
 
 # template
