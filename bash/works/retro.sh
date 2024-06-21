662927dd45fe0a9562dca4df82e41d310b1711c9
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-21 09:05:01 +0200

    Adding Bash Scripts

--- bash/works/retro.sh
+++ bash/works/retro.sh
@@ -3,7 +3,7 @@
 SCRIPT_NAME="works_retro"
 SCRIPT_DESCRIPTION="Install and configure the applications that I use in my hobby of retro computing."
 SCRIPT_DESCRIPTION_ES="Instala y configura las aplicaciones que utilizo en mi afición sobre la retro informática."
-SCRIPT_VERSION="24.06.16"
+SCRIPT_VERSION="24.06.19"
 
 # tools
 . "$(dirname "$0")/../bash-utilities.sh"
@@ -14,5 +14,4 @@ if [ "$ARG_BASH_H" != true ]; then
   "${SCRIPTS_PATH}/lha-app/install.sh" "-y" "-m"
   "${SCRIPTS_PATH}/fs-uae-app/install.sh" "-y" "-m"
   "${SCRIPTS_PATH}/dosbox-app/install.sh" "-y" "-m"
-  "${SCRIPTS_PATH}/mega-app/install.sh" "-y" "-m"
 fi
